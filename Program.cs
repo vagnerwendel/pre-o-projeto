0c795b0ebce16a83ef991cb6c6caf719d5cfc16a
Wendel <wendel.vagner12@gmail.com>
2022-04-21 18:29:45 -0300

    Add projeto

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace preço_codigo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int codigo = int.Parse(valores[0]);
+            int quantidade = int.Parse(valores[1]);
+
+            double total = 0;
+            if (codigo == 1)
+            {
+                total = quantidade * 4.0;
+            }
+            else if (codigo == 2)
+            {
+                total = quantidade * 4.50;
+            }
+            else
+            {
+                total = quantidade * 5.0;
+            }
+
+            Console.WriteLine("Total: " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
